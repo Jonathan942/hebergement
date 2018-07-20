9c38411efb3e55f4684e7111966497c3e62e1067
jonathan942 <jonathan942@hotmail.com>
2018-07-20 10:45:41 +0200

    1er jet : traitement du formulaire d'identification par mail et mot de passe

---
+++ identification.php
@@ -0,0 +1,31 @@
+<?php session_start();
+
+// if nécessaire? (les champs email et mdp sont en "required")
+if (isset($_POST['email']) AND (isset($_POST['mdp']))){
+    $email=htmlspecialchars($_POST['email']);
+    $mdp=htmlspecialchars($_POST['mdp']);
+    if (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#",$email)) {
+        $_SESSION['erreur_identif']="l'adresse email est invalide";
+    } else {
+        include_once('connexion_sql.php');
+        //on vérifie que le mdp correspond au mdp hashé stocké:
+        $recup_mdp=$bdd->prepare('SELECT id_profil, mdp FROM profil WHERE email=? '); 
+        $recup_mdp->execute(array($email)); 
+        $identification=$recup_mdp->fetch();
+
+        if (empty($identification)){
+            $_SESSION['erreur_identif']="erreur d'email";
+        } else {
+            $passVerif = password_verify($mdp, $identification['mdp']);
+            if (!$passVerif)
+            {
+                $_SESSION['erreur_identif']="erreur de mot de passe";
+            } else {
+                $_SESSION['id_profil']=$identification['id_profil'];
+            }
+        } 
+    }
+}
+header('Location: /www/hebergement');
+exit();
+?>
